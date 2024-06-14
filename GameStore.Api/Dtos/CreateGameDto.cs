bc5f8db35bba0b72cb81825b4a219385785ce921
xlixAxilx <sarbunor@gmail.com>
2024-06-14 14:34:57 -0500

    Initial commit

---
+++ GameStore.Api/Dtos/CreateGameDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Api.Dtos;
+
+public record class CreateGameDto(
+    // attribute - property
+    [Required]
+    [StringLength(50)]
+    string Name,
+    
+    [Required]
+    [StringLength(20)]
+    string Genre,
+
+    [Range(1,100)]
+    decimal Price,
+
+    DateOnly ReleaseDate
+    );
+
+// use data notations to guard agains invalid input
